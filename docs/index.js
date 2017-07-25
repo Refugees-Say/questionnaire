e6fe5ca4a32a1d2cb6f7a0c2f5e70548a5751549
Sanjay Guruprasad <sanjay.guruprasad@gmail.com>
2017-07-25 15:29:39 -0400

    Add more questions and loop them

--- docs/index.js
+++ docs/index.js
@@ -26967,7 +26967,7 @@ var QuestionnaireApp = function (_React$Component) {
             question_type: "multi_answer",
             question_text: "Is it important for you to access any of these?",
             question_image: null,
-            next_question_id: "1",
+            next_question_id: "3",
             options: [{
               option_id: "2_1",
               option_text: "English teaching",
@@ -26979,6 +26979,84 @@ var QuestionnaireApp = function (_React$Component) {
             }, {
               option_id: "2_3",
               option_text: "University",
+              option_image: null
+            }]
+          }, {
+            question_id: "3",
+            question_type: "multi_answer",
+            question_text: "Is it important for you to live near any of these places of worship?",
+            question_image: null,
+            next_question_id: "4",
+            options: [{
+              option_id: "3_1",
+              option_text: "Mosque",
+              option_image: null
+            }, {
+              option_id: "3_2",
+              option_text: "Church",
+              option_image: null
+            }, {
+              option_id: "3_3",
+              option_text: "Synagogue",
+              option_image: null
+            }, {
+              option_id: "3_4",
+              option_text: "Hindu Temple",
+              option_image: null
+            }, {
+              option_id: "3_5",
+              option_text: "Sikh Temple",
+              option_image: null
+            }, {
+              option_id: "3_6",
+              option_text: "Buddhist Temple",
+              option_image: null
+            }]
+          }, {
+            question_id: "4",
+            question_type: "multi_answer",
+            question_text: "Which of these sectors would you be most likely to look for a job in?",
+            question_image: null,
+            next_question_id: "1",
+            options: [{
+              option_id: "4_1",
+              option_text: "Administration",
+              option_image: null
+            }, {
+              option_id: "4_2",
+              option_text: "Manufacturing",
+              option_image: null
+            }, {
+              option_id: "4_3",
+              option_text: "Education",
+              option_image: null
+            }, {
+              option_id: "4_4",
+              option_text: "Construction",
+              option_image: null
+            }, {
+              option_id: "4_5",
+              option_text: "Retail",
+              option_image: null
+            }, {
+              option_id: "4_6",
+              option_text: "Health and Social work",
+              option_image: null
+            }, {
+              option_id: "4_7",
+              option_text: "Electricity, Gas and Water",
+              option_image: null
+            }, {
+              option_id: "4_8",
+              option_text: "Hotel and Restaurant",
+              option_image: null
+            }, {
+              option_id: "4_9",
+              option_text: "Agriculture",
+              option_image: null
+            }, {
+              option_id: "4_10",
+              option_text: "Business",
               option_image: null
             }]
           }],
